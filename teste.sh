21ab7f687d6630299378b97f56e2304147e91d73
FABIO NACONECZNY DA SILVA <fns21@inf.ufpr.br>
2024-04-04 00:07:40 -0300

    formatted output

--- teste.sh
+++ teste.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 METRICA="FLOPS_DP"
-<<<<<<< HEAD
+
 CPU=15
-=======
+
 CPU=8
->>>>>>> 9893d061f7a6626bdd765280b7eb0eaf4e9ef180
 
 LIKWID_HOME=/home/soft/likwid
 
 make -B
-<<<<<<< HEAD
 
 for k in $METRICA
 do
@@ -20,13 +18,4 @@ done
 cat FLOPS_DP_SemOtimiz.log | head -n 25 | tail -n 21
 
 cat ${k}_SemOtimiz.log | grep "DP" | awk '!/AVX/ {print $N, "\n+----------------------+--------------+"}' | sed '/^Region/ s/^/\n/'
-
-
-=======
-for k in $METRICA
-do
-    likwid-perfctr -C ${CPU} -g ${k} -m ./perfSL >${k}_SemOtimiz.log
-done
-
->>>>>>> 9893d061f7a6626bdd765280b7eb0eaf4e9ef180
 
